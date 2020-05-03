9a66ae0d0d8f96e3c8873732122cc40c7a61129e
Brion Fuller <blfullerii@gmail.com>
2020-05-02 17:48:22 -0700

    Replace websocket and websocket-server with socket.io 1) game loads but player dies right away 2) still need to clean up ws

--- server/js/formulas.js
+++ server/js/formulas.js
@@ -1,26 +1,23 @@
-
 var Utils = require("./utils");
 
 var Formulas = {};
 
-Formulas.dmg = function(weaponLevel, armorLevel) {
-    var dealt = weaponLevel * Utils.randomInt(5, 10),
-        absorbed = armorLevel * Utils.randomInt(1, 3),
-        dmg =  dealt - absorbed;
-    
-    //console.log("abs: "+absorbed+"   dealt: "+ dealt+"   dmg: "+ (dealt - absorbed));
-    if(dmg <= 0) {
-        return Utils.randomInt(0, 3);
-    } else {
-        return dmg;
-    }
+module.exports = Formulas;
+
+Formulas.dmg = function (weaponLevel, armorLevel) {
+  var dealt = weaponLevel * Utils.randomInt(5, 10),
+    absorbed = armorLevel * Utils.randomInt(1, 3),
+    dmg = dealt - absorbed;
+
+  //console.log("abs: "+absorbed+"   dealt: "+ dealt+"   dmg: "+ (dealt - absorbed));
+  if (dmg <= 0) {
+    return Utils.randomInt(0, 3);
+  } else {
+    return dmg;
+  }
 };
 
-Formulas.hp = function(armorLevel) {
-    var hp = 80 + ((armorLevel - 1) * 30);
-    return hp;
+Formulas.hp = function (armorLevel) {
+  var hp = 80 + (armorLevel - 1) * 30;
+  return hp;
 };
-
-if(!(typeof exports === 'undefined')) {
-    module.exports = Formulas;
-}
